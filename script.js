999257d5fa3d9e86a9e19616377159957de03738
anonuevo-jheferson <jhefersonzanonuevo@gmail.com>
2024-05-02 17:40:43 +0800

    add pause function

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let counter = 0;
 let timer;
 
 let startButton = document.querySelector('#start_button');
+let pauseButton = document.querySelector('#pause_button');
 let displayCounter = document.querySelector('#counter');
 
 function startTimer() {
     timer = setInterval(updateCounter, 1000);
+}
+
+function pauseTimer() {
+    clearInterval(timer);
 }
 
 function updateCounter() {
@@ -14,3 +19,4 @@ function updateCounter() {
 }
 
 startButton.addEventListener('click', startTimer);
+pauseButton.addEventListener('click', pauseTimer);
