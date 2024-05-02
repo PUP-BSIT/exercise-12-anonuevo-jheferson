a7682dbf50d81962eca4c1355b513be38c548a1e
anonuevo-jheferson <jhefersonzanonuevo@gmail.com>
2024-05-02 18:33:33 +0800

    fix indentation

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-let startButton = document.querySelector('#start_button');
-let pauseButton = document.querySelector('#pause_button');
-let resumeButton = document.querySelector('#resume_button');
-let stopButton = document.querySelector('#stop_button');
-let displayCount = document.querySelector('#count');
-
+let startButton = document.querySelector("#start_button");
+let pauseButton = document.querySelector("#pause_button");
+let resumeButton = document.querySelector("#resume_button");
+let stopButton = document.querySelector("#stop_button");
+let displayCount = document.querySelector("#count");
 let count = 0;
 let timer;
 
 function startTimer() {
-    timer = setInterval(updateCount, 1000);
+	timer = setInterval(updateCount, 1000);
 }
 
 function pauseTimer() {
-    clearInterval(timer);
+	clearInterval(timer);
 }
 
 function resumeTimer() {
-    startTimer();
+	startTimer();
 }
 
 function stopTimer() {
-    clearInterval(timer);
-    count = 0;
-    displayCount.innerText = count;
+	clearInterval(timer);
+	count = 0;
+	displayCount.innerText = count;
 }
 
 function updateCount() {
-    count++;
-    displayCount.innerText = count;
+	count++;
+	displayCount.innerText = count;
 }
 
-startButton.addEventListener('click', startTimer);
-pauseButton.addEventListener('click', pauseTimer);
-resumeButton.addEventListener('click', resumeTimer);
-stopButton.addEventListener('click', stopTimer);
+startButton.addEventListener("click", startTimer);
+pauseButton.addEventListener("click", pauseTimer);
+resumeButton.addEventListener("click", resumeTimer);
+stopButton.addEventListener("click", stopTimer);
